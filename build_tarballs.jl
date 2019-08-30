25583cc7a78fd38d828d054456c1a826b0145342
Ian <i.r.butterworth@gmail.com>
2019-08-29 23:45:13 -0400

    add shared-mim-info

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -34,20 +34,21 @@ products(prefix) = [
 # Dependencies that must be installed before this package can be built
 # Based on http://www.linuxfromscratch.org/blfs/view/8.3/x/gdk-pixbuf.html
 dependencies = [
-    # GLib need zlib
+    # GLib needs zlib
     "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl",
-    # GLib need libffi
+    # GLib needs libffi
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Libffi-v3.2.1-0/build_Libffi.v3.2.1.jl",
-    # GLib need gettext
+    # GLib needS gettext
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Gettext-v0.19.8-0/build_Gettext.v0.19.8.jl",
-    # GLib need pcre
+    # GLib needS pcre
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/PCRE-v8.42-2/build_PCRE.v8.42.0.jl",
-    # GLib need iconv
+    # GLib needS iconv
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Libiconv-v1.15-0/build_Libiconv.v1.15.0.jl",
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Glib-v2.59.0%2B0/build_Glib.v2.59.0.jl",
     "https://github.com/JuliaIO/LibpngBuilder/releases/download/v1.0.3/build_libpng.v1.6.37.jl",
-    "https://github.com/JuliaIO/LibJPEGBuilder/releases/download/v10/build_libjpeg.v9.0.0-b.jl",
-    "https://github.com/JuliaIO/LibTIFFBuilder/releases/download/v6/build_libtiff.v4.0.9.jl"
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/JpegTurbo-v2.0.1-0/build_JpegTurbo.v2.0.1.jl",
+    "https://github.com/JuliaIO/LibTIFFBuilder/releases/download/v6/build_libtiff.v4.0.9.jl",
+    "https://github.com/ianshmean/SharedMimeInfoBuilder/releases/download/v1.0.0/build_shared-mime-info.v1.10.0.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
