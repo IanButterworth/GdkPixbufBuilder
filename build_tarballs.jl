2949dd93eb1082a4816d7117de2b6c331af4f94a
Ian <i.r.butterworth@gmail.com>
2019-08-30 17:30:35 -0400

    add x11

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -35,6 +35,7 @@ products(prefix) = [
 # Dependencies that must be installed before this package can be built
 # Based on http://www.linuxfromscratch.org/blfs/view/8.3/x/gdk-pixbuf.html
 dependencies = [
+    "https://github.com/giordano/Yggdrasil/releases/download/X11-v1.6.8/build_X11.v1.6.8.jl",
     # GLib needs zlib
     "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl",
     # GLib needs libffi
@@ -45,7 +46,9 @@ dependencies = [
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/PCRE-v8.42-2/build_PCRE.v8.42.0.jl",
     # GLib needS iconv
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Libiconv-v1.15-0/build_Libiconv.v1.15.0.jl",
+
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Glib-v2.59.0%2B0/build_Glib.v2.59.0.jl",
+
     "https://github.com/JuliaIO/LibpngBuilder/releases/download/v1.0.3/build_libpng.v1.6.37.jl",
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/JpegTurbo-v2.0.1-0/build_JpegTurbo.v2.0.1.jl",
     "https://github.com/JuliaIO/LibTIFFBuilder/releases/download/v6/build_libtiff.v4.0.9.jl",
